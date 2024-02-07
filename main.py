438a2f316753f5175761e970848546599fc9a69d
= <=>
2024-02-07 14:30:44 +0100

    ui improvement

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ from menuBar import MenuBar
 from tkinter import Tk
 
 def main():
-    x = 10005
+    x = 1000
     y = 600
     root = Tk()
     ad = AztecDiamond(x, y)
     tracker = ResizeTracker(ad)
-    root.geometry(str(x)+"x"+str(y)+"+300+300")
-
     menubar = MenuBar(root, ad)
 
+    root.geometry(str(x)+"x"+str(y)+"+300+300")
     root.mainloop()
 
 if __name__ == '__main__':
